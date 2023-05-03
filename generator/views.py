24fcbab43f5c95b614d8aa423d7bee61c576c41a
Mrusi <mrusi@mail.ru>
2023-05-03 12:36:10 +0400

    Our first commit!

---
+++ generator/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+import string
+# Create your views here.
+
+def home(request):
+    return render(request, 'generator/home.html')
+
+def password(request):
+
+    characters = list(string.ascii_lowercase)
+
+    if request.GET.get('uppercase'):
+        characters.extend(list(string.ascii_uppercase))
+
+    if request.GET.get('special'):
+        characters.extend(list(string.punctuation))
+
+    if request.GET.get('numbers'):
+        characters.extend(list(string.digits))
+
+    thepassword = ''
+
+    lenght = int(request.GET.get('lenght',12))
+
+    for i in range(lenght):
+        thepassword += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password' : thepassword})
+
+
+def aboutme(request):
+    return render(request, 'generator/aboutme.html')
